5f0b1f7763ec75905195c630d1d942aa338278ff
laudryfadian <skakmate87@gmail.com>
2022-04-14 07:30:25 +0700

    find product

--- modules/produk/product.route.js
+++ modules/produk/product.route.js
@@ -5,15 +5,10 @@ async function routes(fastify, otps){
   fastify.get("/produk",
     async (req, reply) => {
       try {
-        // const hit = await Produk.findOne({name: req.query.nama}).lean();
-        // if (!hit) {
-        //   return reply.failed("Terjadi kesalahan", 400);
-        // }
-        const transId = req.body.TransId
-
-        console.log("-----------");
-        console.log(transId);
-        console.log("----------------");
+        const hit = await Produk.findOne({name: req.query.nama}).lean();
+        if (!hit) {
+          return reply.failed("Terjadi kesalahan", 400);
+        }
         
         reply.success("Berhasil menampilkan data", "hit");
       } catch (err) {
